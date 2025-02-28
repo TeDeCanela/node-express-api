85f5635f399c5ce1e0d2f3b482c9942394672fee
TeDeCanela <153479319+TeDeCanela@users.noreply.github.com>
2025-02-28 12:08:27 -0600

    Implementado endpoint '/saludo/:nombre'

--- index.js
+++ index.js
@@ -7,3 +7,11 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.send('Bienvenido a la API');
 });
+
+app.get('saludo/:nombre',(req, res) =>{
+    res.send(`Hola, ${req.params.nombre}!`);
+});
+
+app.listen(port, ()=>{
+    console.log(`Servidor escuchando en http://localhost:${port}`)
+});
